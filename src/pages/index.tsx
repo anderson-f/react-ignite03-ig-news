b6b2ef16a154df7e732215fa4944e98ae38fded6
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-06-04 10:53:32 -0300

    feat:Calling stripe api for pricing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
+import { GetStaticProps } from 'next'
+
 // Tudo dentro desse head vai ser anexado ao head do document
 import Head from 'next/head' // posso botar em qualquer lugar da tela
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+// Client-side - não precisa de indexação/ação do usuário/sem necessidade de já ter a informação
+// Server-side - tempo real
+// Static Site Generation - informações fixas
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +30,34 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+/* getServerSideProps GetServerSideProps */
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IycKOBVg5kZNibMGUHrA73M', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
