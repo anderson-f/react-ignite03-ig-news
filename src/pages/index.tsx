3ed3b51e61b709dee2477cf7f3ba65425991ec42
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-06-04 09:08:00 -0300

    feat:Home stylization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 // Tudo dentro desse head vai ser anexado ao head do document
 import Head from 'next/head' // posso botar em qualquer lugar da tela
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello world
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
