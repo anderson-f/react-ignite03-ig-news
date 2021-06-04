6c9e9959e842cc874ce3712b1f3498ac916e8e4b
Anderson Freitas <andersonfreitas.tec@gmail.com>
2021-06-04 09:20:43 -0300

    feat:Subscribe button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // Tudo dentro desse head vai ser anexado ao head do document
 import Head from 'next/head' // posso botar em qualquer lugar da tela
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
